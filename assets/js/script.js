229d1fe9d0cb45ee45a0465d18c4b81bc0b9c355
Roman Yarovoy <roman@booma.co.il>
2020-10-13 10:48:38 +0300

    add_rep

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://http-whitechapel-zzz-com-ua.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+
+$('#burg-menu').on('click',function(){
+  $('.menu-mobile').slideToggle();
+});
